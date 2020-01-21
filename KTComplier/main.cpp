8338ab507c52e7b703aa2fc9f59002b5b2412fb9
黄嘉豪 <huangjiahao.kt@bytedance.com>
2020-01-22 00:37:08 +0800

    Initial Commit

---
+++ KTComplier/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KTComplier
+//
+//  Created by 黄嘉豪 on 2020/1/22.
+//  Copyright © 2020 黄嘉豪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
